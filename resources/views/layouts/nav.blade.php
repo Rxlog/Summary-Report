af1cea3b43bc422dedc329a18abee6c2df418cd8
Nero <reymark.torres08.dev@gmail.com>
2019-01-08 11:46:58 +0800

    Add branding logo in the navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,17 +1,22 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+<nav class="navbar navbar--flat navbar-expand-md navbar-dark bg-primary">
     <div class="container">
-        <a class="navbar-brand" href="{{ route('summary-report') }}">
-            {{ config('app.name', 'Laravel') }}
-        </a>
+        <img class="navbar__image" src="{{ asset('images/branding.jpg') }}" alt="XLOG Logo">
+
+
+
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
             aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
+            <ul class="navbar-nav ml-auto">
+                <li>
+                    <a class="navbar-brand" href="{{ route('summary-report') }}">
+                        {{ config('app.name', 'XLOG REPORT') }}
+                    </a>
+                </li>
             </ul>
 
             <!-- Right Side Of Navbar -->
@@ -30,14 +35,14 @@
                 <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
                         aria-expanded="false" v-pre>
-                                    {{ Auth::user()->email }} <span class="caret"></span>
-                                </a>
+                        {{ Auth::user()->email }} <span class="caret"></span>
+                    </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
 
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             @csrf
